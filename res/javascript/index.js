d3ac3b548fc71a6a94cbb981b2644fd926b0eef8
Adriel007 <adrielsouzaandrade8@gmail.com>
2023-05-23 17:57:20 -0300

    commit

--- res/javascript/index.js
+++ res/javascript/index.js
@@ -1,1 +1,11 @@
-await fetch("res/php/scripts.php?level=5&group=love").then(res => res.json());
+// await fetch("res/php/machine-learning.php?level=5&group=love").then(res => res.json());
+(function () {
+    // Click Event
+    const a = document.querySelectorAll("header nav a");
+    a.forEach(val => {
+        val.onclick = () => {
+            a.forEach(others => others.style.borderBottom = "solid 3px transparent");
+            val.style.borderBottom = "solid 3px #fff";
+        };
+    });
+})();
