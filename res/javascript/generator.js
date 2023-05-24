41026fd5f47f2d96a4ac1384d16414e4791d77c4
Adriel007 <adrielsouzaandrade8@gmail.com>
2023-05-23 23:58:20 -0300

    commit

--- res/javascript/generator.js
+++ res/javascript/generator.js
@@ -1,17 +1,31 @@
 function send() {
-    const text = document.querySelector("textarea").value;
-    const file = new Blob([text], { type: 'text/plain' });
+    const texts = document.querySelectorAll("textarea");
+    let str = "";
+
+    texts.forEach(text => str += text.value + "@separatorphp@");
+
+    const textEncoder = new TextEncoder();
+    const utf8Bytes = textEncoder.encode(str);
+    const file = new Blob([utf8Bytes], { type: 'text/plain;charset=utf-8' });
     const formData = new FormData();
     formData.append('file', file);
+
+    formData.append('level', level = 2);
     fetch('../php/scripts.php', {
         method: 'POST',
         body: formData
     })
-        .then(response => response.text())
+        .then(response => response.json())
         .then(data => {
             console.log(data);
         })
         .catch(error => {
             console.error('Erro:', error);
         });
+}
+
+function newTextArea() {
+    const textarea = document.createElement("textarea");
+
+    document.body.appendChild(textarea);
 }
--- res/php/scripts.php
+++ res/php/scripts.php
@@ -1,28 +1,36 @@
 <?php
 header('Content-Type: application/json; charset=utf-8');
+mb_internal_encoding('UTF-8');
 include(dirname(__DIR__) . '/machine-learning/markov-chain.php');
 error_reporting(0);
 
 if (isset($_FILES['file'])) {
     $tmp = $_FILES['file']['tmp_name'];
     $content = file_get_contents($tmp);
-    $array = explode('\n', $content);
-    markov_chain($arr, 3);
-} else {
-    echo 'Nenhum arquivo foi enviado.';
-}
+    $currentEncoding = mb_detect_encoding($content, 'UTF-8, ISO-8859-1');
+    $content = mb_convert_encoding($content, 'UTF-8', $currentEncoding);
+    $array = explode('@separatorphp@', $content);
+    array_pop($array);
+    $result = [];
 
-$level = !empty($_GET['level']) ? $_GET['level'] : 2;
-$group = !empty($_GET['group']) ? $_GET['group'] : 'love';
+    for ($c = 0; $c < 5; $c++)
+        $result[] = mb_convert_encoding(markov_chain($array, 2)['markov'], 'UTF-8', 'UTF-8');
 
-markov_chain($dataset, $level, $group);
+    $jsonResult = json_encode(['result' => $result], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
 
-function markov_chain($dataSet, $level)
+    if ($jsonResult === false)
+        echo json_encode(['error' => json_last_error_msg()], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+    else
+        echo $jsonResult;
+} else
+    echo json_encode(['error' => 'Nenhum arquivo foi enviado'], JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+
+function markov_chain($dataset, $level)
 {
     $arr = [];
     $mkc = new MarkovChain($level); // 1-2 low cohesion high creativity, 3-4 medium cohesion medium creativity, 5+ high cohesion low creativity
-    $mkc->train($dataSet);
-    $arr['markov'] = $mkc->generateText(100);
+    $mkc->train($dataset);
+    $arr['markov'] = $mkc->generateText(10);
     $arr['score'] = $mkc->scoreGeneratedText($arr['markov']);
-    echo json_encode($arr, JSON_UNESCAPED_UNICODE | JSON_UNESCAPED_SLASHES);
+    return $arr;
 }
